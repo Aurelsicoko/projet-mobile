cb13cc78a2b43bcc530498f67fa75d624f3fba18
souvir <ambalaurelie@gmail.com>
2014-12-15 18:40:01 +0100

    [master] Initial commit with Facebook connect

---
+++ chopacho/ViewController.h
@@ -0,0 +1,25 @@
+//
+//  ViewController.h
+//  chopacho
+//
+//  Created by aurelie ambal on 15/12/2014.
+//  Copyright (c) 2014 hetic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <FacebookSDK/FacebookSDK.h>
+
+@interface ViewController : UIViewController <FBLoginViewDelegate>
+
+@property (weak, nonatomic) IBOutlet FBLoginView *loginView;
+
+@property (weak, nonatomic) IBOutlet UILabel *lblLoginStatus;
+
+@property (weak, nonatomic) IBOutlet UILabel *lblUsername;
+
+@property (weak, nonatomic) IBOutlet UILabel *lblEmail;
+
+@property (weak, nonatomic) IBOutlet FBProfilePictureView *profilePicture;
+
+@end
+
---
+++ chopacho/ViewController.m
@@ -0,0 +1,104 @@
+//
+//  ViewController.m
+//  chopacho
+//
+//  Created by aurelie ambal on 15/12/2014.
+//  Copyright (c) 2014 hetic. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+-(void)toggleHiddenState:(BOOL)shouldHide;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    [self toggleHiddenState:YES];
+    self.lblLoginStatus.text = @"";
+    
+    self.loginView.readPermissions = @[@"public_profile", @"email", @"user_friends"];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Private method implementation
+
+-(void)toggleHiddenState:(BOOL)shouldHide{
+    self.lblUsername.hidden = shouldHide;
+    self.lblEmail.hidden = shouldHide;
+    self.profilePicture.hidden = shouldHide;
+}
+
+
+#pragma mark - FBLoginView Delegate method implementation
+
+-(void)loginViewShowingLoggedInUser:(FBLoginView *)loginView{
+    self.lblLoginStatus.text = @"You are logged in.";
+    
+    [self toggleHiddenState:NO];
+}
+
+
+-(void)loginViewFetchedUserInfo:(FBLoginView *)loginView user:(id<FBGraphUser>)user{
+    NSLog(@"%@", user);
+    self.profilePicture.profileID = user.objectID;
+    self.lblUsername.text = user.name;
+    self.lblEmail.text = [user objectForKey:@"email"];
+}
+
+
+-(void)loginViewShowingLoggedOutUser:(FBLoginView *)loginView{
+    self.lblLoginStatus.text = @"You are logged out";
+    
+    [self toggleHiddenState:YES];
+}
+
+
+- (void)loginView:(FBLoginView *)loginView handleError:(NSError *)error
+{
+    NSString *alertMessage, *alertTitle;
+    
+    if ([FBErrorUtility shouldNotifyUserForError:error])
+    {
+        alertTitle = @"Facebook error";
+        alertMessage = [FBErrorUtility userMessageForError:error];
+        
+    }
+    else if ([FBErrorUtility errorCategoryForError:error] == FBErrorCategoryAuthenticationReopenSession)
+    {
+        alertTitle = @"Session Error";
+        alertMessage = @"Your current session is no longer valid. Please log in again.";
+        
+    }
+    else if ([FBErrorUtility errorCategoryForError:error] == FBErrorCategoryUserCancelled)
+    {
+        NSLog(@"user cancelled login");
+        
+    }
+    else
+    {
+        alertTitle  = @"Something went wrong";
+        alertMessage = @"Please try again later.";
+        NSLog(@"Unexpected error:%@", error);
+    }
+    
+    if (alertMessage)
+    {
+        [[[UIAlertView alloc] initWithTitle:alertTitle
+                                    message:alertMessage
+                                   delegate:nil
+                          cancelButtonTitle:@"OK"
+                          otherButtonTitles:nil] show];
+    }
+}
+
+@end
