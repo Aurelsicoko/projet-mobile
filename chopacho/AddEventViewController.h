660a856ae7029715c2897a0115839e96cda45365
Aurélien Georget <aurelsicoko@gmail.com>
2014-12-16 11:54:32 +0100

    [master] Add UI el to class

--- chopacho/AddEventViewController.h
+++ chopacho/AddEventViewController.h
@@ -9,5 +9,9 @@
 #import <UIKit/UIKit.h>
 
 @interface AddEventViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITextField *titleEventTextField;
+@property (weak, nonatomic) IBOutlet UITextView *descriptionTextView;
+@property (weak, nonatomic) IBOutlet UISearchBar *inviteSearchBar;
+@property (weak, nonatomic) IBOutlet UIButton *choiceThemeButton;
 
 @end
